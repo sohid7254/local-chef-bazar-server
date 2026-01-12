7832c3f1baf53207124fec14b9f246e369007802
sohid7254 <sohidameen321@gmail.com>
2026-01-12 19:01:14 +0600

    added profile updated logic

--- index.js
+++ index.js
@@ -131,6 +131,31 @@ app.post("/users", async (req, res) => {
     const result = await usersCollection.insertOne(user);
     res.send(result);
 });
+// update user profile
+app.patch("/users/:email", verifyFBToken, async (req, res) => {
+  const email = req.params.email;
+  const updatedData = req.body;
+
+  if (req.decoded_email !== email) {
+    return res.status(403).send({ error: "Access Denied" });
+  }
+
+  try {
+    const result = await usersCollection.updateOne(
+      { email },
+      { $set: updatedData }
+    );
+
+    if (result.modifiedCount > 0) {
+      res.send({ success: true, message: "Profile updated successfully" });
+    } else {
+      res.send({ success: false, message: "No changes applied" });
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
+});
+
 // get all users on frontend
 app.get("/users", verifyFBToken, verifyAdmin, async (req, res) => {
     const allUsers = await usersCollection.find().sort({ createdAt: -1 }).toArray();
